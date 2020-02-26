2a6ece4da88a8c4b74d93e132fcb712c69dc2973
Vasiliy Zhigalov <vasiliyzhigalov@gmail.com>
2020-02-26 20:51:43 +0400

    Эмулятор АТМ

---
+++ hw06-ATM/src/main/java/com/vasiliyzhigalov/atm/ATM.java
@@ -0,0 +1,18 @@
+package com.vasiliyzhigalov.atm;
+
+
+import com.vasiliyzhigalov.atm.exceptions.ATMImpossibleGiveAmountException;
+import com.vasiliyzhigalov.atm.exceptions.ATMСellOverflowException;
+
+import java.util.*;
+
+public interface ATM {
+
+    void addCashBox();
+
+    int getBalance();
+
+    void addCash(List<Banknote> cash) throws ATMСellOverflowException;
+
+    List<Banknote> getCash(int sum) throws ATMImpossibleGiveAmountException;
+}

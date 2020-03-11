febb285daebc66f2b57eee8367becd3afd82f0e8
Vasiliy Zhigalov <40051520+VasiliyZhigalov@users.noreply.github.com>
2020-03-11 20:31:21 +0400

    Delete ATMImpossibleGiveAmountException.java

--- hw07-departmentATM/src/main/java/com/vasiliyzhigalov/department/atm/exceptions/ATMImpossibleGiveAmountException.java
+++
@@ -1,7 +0,0 @@
-package com.vasiliyzhigalov.department.atm.exceptions;
-
-public class ATMImpossibleGiveAmountException extends ATMException {
-    public ATMImpossibleGiveAmountException(String message) {
-        super(message);
-    }
-}

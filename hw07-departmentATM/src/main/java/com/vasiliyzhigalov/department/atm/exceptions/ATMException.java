1da8ce46d3d06ca160831fdc7bbeffbecd793506
Vasiliy Zhigalov <40051520+VasiliyZhigalov@users.noreply.github.com>
2020-03-11 20:30:46 +0400

    Delete ATMException.java

--- hw07-departmentATM/src/main/java/com/vasiliyzhigalov/department/atm/exceptions/ATMException.java
+++
@@ -1,7 +0,0 @@
-package com.vasiliyzhigalov.department.atm.exceptions;
-
-public abstract class ATMException extends Exception {
-    public ATMException(String message) {
-        super(message);
-    }
-}
